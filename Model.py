b3e2f627bc204d7ef2465a63a2ec63cd302c947c
Hazem Samoaa <37386593+hazemsamoaa@users.noreply.github.com>
2019-08-17 15:52:04 +0200

    Add files via upload

---
+++ Model.py
@@ -0,0 +1,180 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 16 21:05:09 2019
+
+@author: Peter Samoaa
+"""
+
+
+
+#### Importing Libraries ####
+
+import pandas as pd
+import numpy as np
+import seaborn as sn
+import matplotlib.pyplot as plt
+import time
+
+dataset = pd.read_csv('new_appdata10.csv')
+
+
+#### Data Pre-Processing ####
+
+# Splitting Independent and Response Variables
+response = dataset["enrolled"]
+dataset = dataset.drop(columns="enrolled")
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(dataset, response,
+                                                    test_size = 0.2,
+                                                    random_state = 0)
+
+## Balancing the Training Set
+#import random
+#y_train.value_counts()
+#
+#pos_index = y_train[y_train.values == 1].index
+#neg_index = y_train[y_train.values == 0].index
+#
+#if len(pos_index) > len(neg_index):
+#    higher = pos_index
+#    lower = neg_index
+#else:
+#    higher = neg_index
+#    lower = pos_index
+#
+#random.seed(0)
+#higher = np.random.choice(higher, size=len(lower))
+#lower = np.asarray(lower)
+#new_indexes = np.concatenate((lower, higher))
+#
+#X_train = X_train.loc[new_indexes,]
+#y_train = y_train[new_indexes]
+
+
+# Removing Identifiers for temporal purpose because at the end of the model we want to associate prediction to each user.
+train_identity = X_train['user']
+X_train = X_train.drop(columns = ['user'])
+test_identity = X_test['user']
+X_test = X_test.drop(columns = ['user'])
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train2 = pd.DataFrame(sc_X.fit_transform(X_train)) # here we use dataframe because scaler return an array, whih remove the index (which requiered in order to map with user id) and column names which we need in our model training
+X_test2 = pd.DataFrame(sc_X.transform(X_test))
+X_train2.columns = X_train.columns.values
+X_test2.columns = X_test.columns.values # assign columns to the new testing set
+X_train2.index = X_train.index.values # assign columns to the new training set
+X_test2.index = X_test.index.values
+X_train = X_train2 # compare original training set with new training set.
+X_test = X_test2
+
+# Now all the features have normilized 
+
+#### Model Building ####
+
+
+# Fitting Model to the Training Set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0, penalty = 'l1') # the penalty changed the model from regular logistic regression into regularized 
+# even we handle the screen correlation by funnels, but it maybe also the funnels correlated to each other. So L1 which called Lasso, it penalizes any particular field that it's extremly correlated to the responsible variable. 
+classifier.fit(X_train, y_train)
+
+# Predicting Test Set
+y_pred = classifier.predict(X_test)
+
+# Evaluating Results
+from sklearn.metrics import confusion_matrix, accuracy_score, f1_score, precision_score, recall_score
+cm = confusion_matrix(y_test, y_pred)
+accuracy_score(y_test, y_pred) # how accurate the model is 
+precision_score(y_test, y_pred) # tp / (tp + fp) # check the overfitting 
+recall_score(y_test, y_pred) # tp / (tp + fn) # out of actual positive observation how many we predict them as positive.
+f1_score(y_test, y_pred)
+
+# plot confustion matrix 
+df_cm = pd.DataFrame(cm, index = (0, 1), columns = (0, 1))
+plt.figure(figsize = (10,7))
+sn.set(font_scale=1.4)
+sn.heatmap(df_cm, annot=True, fmt='g')
+print("Test Data Accuracy: %0.4f" % accuracy_score(y_test, y_pred))
+
+
+# Applying k-Fold Cross Validation
+
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
+print("SVM Accuracy: %0.3f (+/- %0.3f)" % (accuracies.mean(), accuracies.std() * 2))
+
+# Analyzing Coefficients
+pd.concat([pd.DataFrame(dataset.drop(columns = 'user').columns, columns = ["features"]),
+           pd.DataFrame(np.transpose(classifier.coef_), columns = ["coef"])
+           ],axis = 1)
+
+
+#### Model Tuning ####
+
+## Grid Search (Round 1)
+from sklearn.model_selection import GridSearchCV
+
+# Select Regularization Method
+penalty = ['l1', 'l2']
+
+# Create regularization hyperparameter space
+C = [0.001, 0.01, 0.1, 1, 10, 100, 1000]
+
+# Combine Parameters
+parameters = dict(C=C, penalty=penalty)
+
+grid_search = GridSearchCV(estimator = classifier,
+                           param_grid = parameters,
+                           scoring = "accuracy",
+                           cv = 10,
+                           n_jobs = -1)
+t0 = time.time()
+grid_search = grid_search.fit(X_train, y_train)
+t1 = time.time()
+print("Took %0.2f seconds" % (t1 - t0))
+
+rf_best_accuracy = grid_search.best_score_
+rf_best_parameters = grid_search.best_params_
+rf_best_accuracy, rf_best_parameters
+
+
+## Grid Search (Round 2)
+
+# Select Regularization Method
+penalty = ['l1', 'l2']
+
+# Create regularization hyperparameter space
+C = [0.1, 0.5, 0.9, 1, 2, 5]
+
+# Combine Parameters
+parameters = dict(C=C, penalty=penalty)
+
+grid_search = GridSearchCV(estimator = classifier,
+                           param_grid = parameters,
+                           scoring = "accuracy",
+                           cv = 10,
+                           n_jobs = -1)
+t0 = time.time()
+grid_search = grid_search.fit(X_train, y_train)
+t1 = time.time()
+print("Took %0.2f seconds" % (t1 - t0))
+
+rf_best_accuracy = grid_search.best_score_
+rf_best_parameters = grid_search.best_params_
+rf_best_accuracy, rf_best_parameters
+grid_search.best_score_
+
+
+#### End of Model ####
+
+
+# Formatting Final Results
+final_results = pd.concat([y_test, test_identity], axis = 1).dropna()
+final_results['predicted_reach'] = y_pred
+final_results = final_results[['user', 'enrolled', 'predicted_reach']].reset_index(drop=True) # we don't want any index 
+
+
